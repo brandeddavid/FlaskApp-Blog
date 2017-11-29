cb65d501dd4100f6a052e4efe4df0e5fd90d9f67
DavidMwangi <david.mathenge98@gmail.com>
2017-11-29 17:31:56 +0300

    Add route to index page

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app.debug = True
+
+@app.route('/')
+
+def index():
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
 
