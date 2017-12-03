713317fc13db26885f049ab36440f88a582761a2
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-12-03 14:23:30 +0300

    Add data file

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+
+    articles = [
+
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'David Mwangi',
+            'create_date':'03/12/17'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'David Mwangi',
+            'create_date':'03/12/17'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author':'David Mwangi',
+            'create_date':'03/12/17'
+        }
+    ]
+
+    return articles
